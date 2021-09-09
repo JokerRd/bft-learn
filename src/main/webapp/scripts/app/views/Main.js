2b12fa435b38e005772a8d51634f427aa8782ee5
BFT\v.konkov <v.a.konkov@mail.ru>
2021-09-09 16:27:02 +0500

    Создал и отцентровал две панели

--- src/main/webapp/scripts/app/views/Main.js
+++ src/main/webapp/scripts/app/views/Main.js
@@ -1,19 +1,14 @@
 Ext.define('Constructor.views.Main', {
     alias: 'main',
-    extend: 'Ext.tab.Panel',
-    title: 'Главная панель',
-    width: 400,
-    height: 200,
-    layout: 'column',
+    extend: 'Ext.panel.Panel',
+    title: 'Конструктор опросов',
+    layout: 'center',
     items: [
-        Ext.create('Ext.tab.Panel', {
-            title: 'Панель 1',
-            width: 500,
-            height: 100,
-        }),
-        Ext.create('Ext.tab.Panel', {
-            title: 'Панель 2',
-            height: 100,
+        Ext.create('Ext.panel.Panel', {
+            title: 'Создание теста',
+            width: '70%',
+            height: '100%',
+            margin: '5 5 5 5'
         })
     ]
 });
