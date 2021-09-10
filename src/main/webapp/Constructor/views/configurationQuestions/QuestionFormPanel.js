d3e88c244e94233da4e7c300528bc92b8efd9b48
BFT\v.konkov <v.a.konkov@mail.ru>
2021-09-10 14:25:01 +0500

    Добавление полей для создания вопроса

--- src/main/webapp/Constructor/views/configurationQuestions/QuestionFormPanel.js
+++ src/main/webapp/Constructor/views/configurationQuestions/QuestionFormPanel.js
@@ -1,7 +1,24 @@
 Ext.define('Constructor.views.configurationQuestions.QuestionFormPanel', {
     extend: 'Ext.form.Panel',
     alias: 'widget.question',
+    requires: [
+        'Constructor.views.configurationQuestions.TypeQuestionComboBox'
+    ],
     title: 'Вопрос',
     width: '100%',
-    height: 100
+    items: [
+        {
+            xtype: 'textfield',
+            fieldLabel: 'Название вопроса',
+            name: 'nameQuestion'
+        },
+        {
+            xtype: 'textareafield',
+            fieldLabel: 'Описание вопроса',
+            name: 'descriptionQuestion'
+        },
+        {
+            xtype: 'type-question'
+        }
+    ]
 })
---
+++ src/main/webapp/Constructor/views/configurationQuestions/TypeQuestionComboBox.js
@@ -0,0 +1,19 @@
+let state = Ext.create('Ext.data.Store', {
+    fields: ['typeQuestion'],
+    data: [
+        {'typeQuestion':'С вводимым ответом'},
+        {'typeQuestion':'C выбором ответа'}
+    ]
+})
+
+
+Ext.define('Constructor.views.configurationQuestions.TypeQuestionComboBox', {
+    extend: 'Ext.form.ComboBox',
+    alias: 'widget.type-question',
+    fieldLabel: 'Тип вопроса',
+    name: 'typeQuestion',
+    store: state,
+    displayField: 'typeQuestion',
+    valueField: 'typeQuestion',
+    queryMode: 'local'
+})
