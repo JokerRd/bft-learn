0c5f71911fdd95960aa299a546ec915cfe17b2a4
BFT\v.konkov <v.a.konkov@mail.ru>
2021-09-10 15:05:52 +0500

    Добавил кнопки удаления вопроса и ответа

---
+++ src/main/webapp/Constructor/views/configurationQuestions/DeleteAnswerButton.js
@@ -0,0 +1,7 @@
+Ext.define('Constructor.views.configurationQuestions.DeleteAnswerButton', {
+    extend: 'Ext.Button',
+    alias: 'widget.delete-answer-btn',
+    text: 'Удалить ответ',
+    margin: '5 5 5 5',
+    height: 50
+})
---
+++ src/main/webapp/Constructor/views/configurationQuestions/DeleteQuestionButton.js
@@ -0,0 +1,7 @@
+Ext.define('Constructor.views.configurationQuestions.DeleteQuestionButton', {
+    extend: 'Ext.Button',
+    alias: 'widget.delete-question-btn',
+    text: 'Удалить вопрос',
+    margin: '5 5 5 5',
+    height: 50
+})
