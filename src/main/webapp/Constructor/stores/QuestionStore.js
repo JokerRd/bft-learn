007a83566ee7fc7e37f117f696f2bda2443ac394
BFT\v.konkov <v.a.konkov@mail.ru>
2021-09-13 20:50:56 +0500

    Подключение считывания данных из локального json

--- src/main/webapp/Constructor/stores/QuestionStore.js
+++ src/main/webapp/Constructor/stores/QuestionStore.js
@@ -1,4 +1,12 @@
 Ext.define('Constructor.stores.QuestionStore', {
     extend: 'Ext.data.Store',
-    model: 'Constructor.models.QuestionModel'
+    model: 'Constructor.models.QuestionModel',
+    proxy: {
+        type: 'ajax',
+        url: 'questions.json',
+        reader: {
+            type: 'json',
+            rootProperty: 'questions'
+        }
+    }
 });
