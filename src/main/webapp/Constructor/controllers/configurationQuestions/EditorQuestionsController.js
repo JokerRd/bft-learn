7cb4f7ce3fb8a58140e7361d90bee10a0862f0f4
BFT\v.konkov <v.a.konkov@mail.ru>
2021-09-13 20:12:50 +0500

    Реализация создания нового вопроса

--- src/main/webapp/Constructor/controllers/configurationQuestions/EditorQuestionsController.js
+++ src/main/webapp/Constructor/controllers/configurationQuestions/EditorQuestionsController.js
@@ -2,7 +2,7 @@ Ext.define('Constructor.controllers.configurationQuestions.EditorQuestionsContro
     extend: 'Ext.app.ViewController',
     alias: 'controller.editor-questions-controller',
     onAddQuestionClick:function ()  {
-        let window = this.lookupReference('question-window');
+        let window = Ext.create('Constructor.views.editorQuestions.QuestionWindow');
         window.show();
     }
 });
