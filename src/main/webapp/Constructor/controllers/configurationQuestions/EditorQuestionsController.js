13a7dd6150aefe929f72191353a9d90f680fd1d0
BFT\v.konkov <v.a.konkov@mail.ru>
2021-09-14 14:50:27 +0500

    Открытие окна редактирования вопроса

--- src/main/webapp/Constructor/controllers/configurationQuestions/EditorQuestionsController.js
+++ src/main/webapp/Constructor/controllers/configurationQuestions/EditorQuestionsController.js
@@ -6,6 +6,16 @@ Ext.define('Constructor.controllers.configurationQuestions.EditorQuestionsContro
         window.show();
     },
     onEditQuestionDoubleClick: function (dv, record, item, index, e) {
-        console.log(dv);
+        let form = Ext.create('Constructor.views.editorQuestions.configurationQuestions.QuestionFormPanel');
+        let id = record.data.id;
+        let store = Ext.data.StoreManager.lookup('question-store');
+        let element  = store.getById(id);
+        console.log(element.data);
+        form.getForm().setValues(element.data);
+        console.log(form);
+        let window = Ext.create('Constructor.views.editorQuestions.QuestionWindow', {
+            items: form
+        });
+        window.show();
     }
 });
