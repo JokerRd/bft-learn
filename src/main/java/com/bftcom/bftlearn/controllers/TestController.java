d252506c846c341ce9341467c65aacbecd6ec9b2
BFT\v.konkov <v.a.konkov@mail.ru>
2021-09-16 16:30:29 +0500

    Тестовая переделка

--- src/main/java/com/bftcom/bftlearn/controllers/TestController.java
+++ src/main/java/com/bftcom/bftlearn/controllers/TestController.java
@@ -3,7 +3,7 @@ package com.bftcom.bftlearn.controllers;
 import com.bftcom.bftlearn.dto.test.NewTest;
 import com.bftcom.bftlearn.dto.test.TestDto;
 import com.bftcom.bftlearn.dto.test.UpdateTest;
-import com.bftcom.bftlearn.services.TestService;
+import com.bftcom.bftlearn.services.TestServiceAbs;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,32 +20,32 @@ import java.util.List;
 @Validated
 public class TestController {
 
-    private final TestService testService;
+    private final TestServiceAbs testService;
 
     @GetMapping
     @Operation(summary = "Получить все тесты")
     public List<TestDto> getAllTest() {
-        return testService.getAllTest();
+        return testService.getAllEntities();
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Получить тест по id")
     public TestDto getTest(@PathVariable(name = "id")
                            @Min(value = 1, message = "Неверный id") long id) {
-        return testService.getTest(id);
+        return testService.getEntity(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "Создать тест")
     public void createTest(@Validated({NewTest.class}) @RequestBody TestDto newTest) {
-        testService.createTest(newTest);
+        testService.createEntity(newTest);
     }
 
     @PutMapping()
     @Operation(summary = "Обновить тест")
     public void updateTest(@Validated({UpdateTest.class}) @RequestBody TestDto updatedTest) {
-        testService.updateTest(updatedTest);
+        testService.updateEntity(updatedTest);
     }
 }
 
