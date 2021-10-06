210fcfa03dd7357786648a8594ea3b70053afc91
Коньков Владислав Александрович <v.konkov@bftcom.com>
2021-10-06 10:52:48 +0500

    Добавление пользователей

--- src/main/java/com/bftcom/bftlearn/configuration/SecurityConfiguration.java
+++ src/main/java/com/bftcom/bftlearn/configuration/SecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.bftcom.bftlearn.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +25,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorizeRequestCustom::customize)
-                .formLogin(formLoginConfigurerCustom);
+                .formLogin(formLoginConfigurerCustom)
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder().encode("adminPassword"))
+                .roles("ADMIN");
     }
 }
