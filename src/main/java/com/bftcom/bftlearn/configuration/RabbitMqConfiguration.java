8b9d1b8ca02068c9cab20f9755a21d1d01b2f903
Коньков Владислав Александрович <v.konkov@bftcom.com>
2021-10-08 16:10:13 +0500

    Исправление конфигурации

--- src/main/java/com/bftcom/bftlearn/configuration/RabbitMqConfiguration.java
+++ src/main/java/com/bftcom/bftlearn/configuration/RabbitMqConfiguration.java
@@ -2,7 +2,9 @@ package com.bftcom.bftlearn.configuration;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMqConfiguration
 {
 
